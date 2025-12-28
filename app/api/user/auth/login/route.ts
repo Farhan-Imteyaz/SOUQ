31b504234c2783083ff5c362ffc24d39c6729132
Faheem <faheemk793@gmail.com>
2025-12-29 01:48:17 +0530

    auth updated

--- app/api/user/auth/login/route.ts
+++ app/api/user/auth/login/route.ts
@@ -3,32 +3,38 @@ import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET!; 
+const SECRET = process.env.JWT_SECRET!;
 export async function POST(req: NextRequest) {
   try {
     const { email, password } = await req.json();
 
     if (!email || !password) {
-      return NextResponse.json({ error: "Email and password required" }, { status: 400 });
+      return NextResponse.json(
+        { error: "Email and password required" },
+        { status: 400 }
+      );
     }
 
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Invalid Email or Password" },
+        { status: 404 }
+      );
     }
 
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
-      return NextResponse.json({ error: "Invalid password" }, { status: 401 });
+      return NextResponse.json(
+        { error: "Invalid Email or Password" },
+        { status: 401 }
+      );
     }
 
-    // Create JWT token
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      SECRET,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {
+      expiresIn: "7d",
+    });
 
     // Set token in an httpOnly cookie
     const response = NextResponse.json({ message: "Login successful" });
@@ -38,7 +44,7 @@ export async function POST(req: NextRequest) {
       httpOnly: true,
       secure: false,
       path: "/",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
+      maxAge: 60 * 60 * 24 * 7,
     });
 
     return response;
