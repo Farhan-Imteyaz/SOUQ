6d053e2cc45bafcfb4c640df6feba660eb59d6c6
Faheem <faheemk793@gmail.com>
2025-12-29 01:49:23 +0530

    login form and route updated

---
+++ app/(user)/(auth)/login/form.tsx
@@ -0,0 +1,150 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Mail, Lock } from "lucide-react";
+import { toast } from "sonner";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { Icon } from "@/app/register/form";
+import { useState } from "react";
+const loginSchema = z.object({
+  email: z.string().email("Enter a valid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+type LoginFormType = z.infer<typeof loginSchema>;
+
+const fieldOrder: (keyof LoginFormType)[] = ["email", "password"];
+const Form = () => {
+  const router = useRouter();
+  const [status, setStatus] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginFormType) => {
+    try {
+      const res = await axios.post("/api/user/auth/login", data, {
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        toast.success("Login Successful", {
+          className:
+            "border !border-green-500/10 !bg-green-500/60 backdrop-blur-lg",
+        });
+        setStatus("ok");
+        router.push("/");
+        return;
+      }
+      toast.error("Invalid email or password", {
+        className: "border !border-red-500/10 !bg-red-500/60 backdrop-blur-sm",
+      });
+      setStatus("error");
+    } catch (error: any) {
+      toast.error(error.response.data.error, {
+        className: "border !border-red-500/10 !bg-red-500/60 backdrop-blur-sm",
+      });
+      setStatus("error");
+    }
+  };
+
+  const onError = (errors: any) => {
+    for (const field of fieldOrder) {
+      const error = errors[field];
+      if (error?.message) {
+        toast.error(error.message, {
+          className:
+            "border !border-red-500/10 !bg-red-500/60 backdrop-blur-lg",
+        });
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)} className="space-y-5">
+      <div>
+        <Label className="block text-sm font-semibold text-gray-700 mb-2">
+          Email Address *
+        </Label>
+        <div className="relative">
+          <Icon
+            icon={<Mail />}
+            className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400"
+            error={errors.email ? true : false}
+          />
+          <Input
+            type="email"
+            {...register("email")}
+            className={`pl-10 ${
+              errors.email &&
+              "focus:border-red-500 focus:outline-red-500 border-red-200/60 bg-red-500/10 placeholder:text-red-700"
+            }`}
+            placeholder="john@example.com"
+          />
+        </div>
+      </div>
+
+      {/* Password */}
+      <div>
+        <Label className="block text-sm font-semibold text-gray-700 mb-2">
+          Password *
+        </Label>
+        <div className="relative">
+          <Icon
+            icon={<Lock />}
+            className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400"
+            error={errors.email ? true : false}
+          />
+          <Input
+            type="password"
+            {...register("password")}
+            className={`pl-10 ${
+              errors.password &&
+              "focus:border-red-500 focus:outline-red-500 border-red-200/60 bg-red-500/10 placeholder:text-red-700"
+            }`}
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="text-right text-sm mt-2 text-blue-500">
+          <Link href={"/forget-password"}>Forgot Password?</Link>
+        </div>
+      </div>
+
+      <Button
+        isLoading={isSubmitting}
+        variant={status === "error" ? "destructive" : "default"}
+        className={`w-full h-11 `}
+        onClick={handleSubmit(onSubmit, onError)}
+      >
+        Log in
+      </Button>
+
+      {/* Sign Up Link */}
+      <p className="text-center text-gray-600">
+        Don't have an account?{" "}
+        <Link
+          href="/register"
+          className="text-blue-600 font-semibold hover:underline"
+        >
+          Create account
+        </Link>
+      </p>
+    </form>
+  );
+};
+
+export default Form;
--- app/(user)/(auth)/login/page.tsx
+++ app/(user)/(auth)/login/page.tsx
@@ -1,65 +1,8 @@
-"use client";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Mail, Lock } from "lucide-react";
-import { toast } from "sonner";
-import axios from "axios";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
-import { Icon } from "@/app/register/form";
-const loginSchema = z.object({
-  email: z.string().email("Enter a valid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-});
-
-type LoginFormType = z.infer<typeof loginSchema>;
-
-const fieldOrder: (keyof LoginFormType)[] = ["email", "password"];
-
-const Page = () => {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = async (data: LoginFormType) => {
-    try {
-      const res = await axios.post("/api/user/login", data, {
-        withCredentials: true,
-      });
-
-      console.log("Login Success:", res.data);
-
-      router.push("/dashboard");
-    } catch (error: any) {
-      console.error("Login Error:", error);
-    }
-  };
-
-  const onError = (errors: any) => {
-    for (const field of fieldOrder) {
-      const error = errors[field];
-      if (error?.message) {
-        toast.error(error.message, {
-          className:
-            "border !border-red-500/10 !bg-red-500/30 backdrop-blur-lg",
-        });
-      }
-    }
-  };
-
+import Form from "./form";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export default async function Page() {
+  
   return (
     <div className="min-h-screen bg-linear-to-br from-blue-50 via-white to-purple-50">
       <div className="container mx-auto px-4 py-12">
@@ -80,70 +23,10 @@ const Page = () => {
                 Sign in to your account
               </h2>
             </div>
-
-            <form
-              onSubmit={handleSubmit(onSubmit, onError)}
-              className="space-y-5"
-            >
-              <div>
-                <Label className="block text-sm font-semibold text-gray-700 mb-2">
-                  Email Address *
-                </Label>
-                <div className="relative">
-                  <Icon
-                    icon={<Mail />}
-                    className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400"
-                    error={errors.email ? true : false}
-                  />
-                  <Input
-                    type="email"
-                    {...register("email")}
-                    required
-                    className={`pl-10 ${
-                      errors.email &&
-                      "focus:border-red-500 focus:outline-red-500 border-red-200/60 bg-red-500/10 placeholder:text-red-700"
-                    }`}
-                    placeholder="john@example.com"
-                  />
-                </div>
-              </div>
-
-              {/* Password */}
-              <div>
-                <Label className="block text-sm font-semibold text-gray-700 mb-2">
-                  Password *
-                </Label>
-                <div className="relative">
-                  <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
-                  <Input
-                    type="password"
-                    {...register("password")}
-                    className={`pl-10 ${
-                      errors.password &&
-                      "focus:border-red-500 focus:outline-red-500 border-red-200/60 bg-red-500/10 placeholder:text-red-700"
-                    }`}
-                    placeholder="Enter your password"
-                  />
-                </div>
-              </div>
-
-              <Button className={"w-full h-11"}>Log in</Button>
-
-              {/* Sign Up Link */}
-              <p className="text-center text-gray-600">
-                Don't have an account?{" "}
-                <Link
-                  href="/register"
-                  className="text-blue-600 font-semibold hover:underline"
-                >
-                  Create account
-                </Link>
-              </p>
-            </form>
+            <Form />
           </div>
         </div>
       </div>
     </div>
   );
-};
-export default Page;
+}
