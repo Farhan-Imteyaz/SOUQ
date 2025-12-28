5444c5432fa9df47760058315314d92801313a0e
Faheem <faheemk793@gmail.com>
2025-12-29 02:26:01 +0530

    status 200 added

--- app/api/user/auth/logout/route.ts
+++ app/api/user/auth/logout/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const response = NextResponse.json({ message: "Logged out" });
+  const response = NextResponse.json({ message: "Logged out", status: 200 });
 
   response.cookies.set("token", "", {
     httpOnly: true,
     expires: new Date(0),
-     secure: false, // <-- must be false on HTTP
+    secure: false,
     path: "/",
   });
 
