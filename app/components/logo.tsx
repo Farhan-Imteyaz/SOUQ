20bb0068d3dd1a46029ef1a8d32e2b8a3aa647eb
Faheem <faheemk793@gmail.com>
2026-01-03 19:08:34 +0530

    classname added

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { Package } from "lucide-react";
-const Logo = () => {
+
+const Logo = ({ className }: { className?: string }) => {
   return (
-    <div className="flex items-center gap-3">
+    <div className={`flex items-center gap-3 ${className}`}>
       <Package className="h-6 w-6 sm:h-8 sm:w-8" />
       <span className="text-lg sm:text-xl font-bold">ParcelForward</span>
     </div>
