35f633d74ac3cd2cf38c6f4011e1ebeca5d812fe
Faheem <faheemk793@gmail.com>
2026-01-26 03:33:18 +0530

    fixed send email

--- lib/sendEmail.tsx
+++ lib/sendEmail.tsx
@@ -1,4 +1,4 @@
-import { resend } from "./resend";
+import { getResendClient } from "./resend";
 import { emailTemplates, EmailType } from "@/emails";
 import { ComponentProps } from "react";
 
@@ -18,7 +18,7 @@ export async function sendEmail<T extends EmailType>({
   props,
 }: SendEmailParams<T>) {
   const { component: Component, subject } = emailTemplates[type];
-
+  const resend = getResendClient();
   return resend.emails.send({
     from: "Souqza <no-reply@souqza.com>",
     to,
